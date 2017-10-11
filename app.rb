f3c966e1a2db154781c390b4497c2a1131af687c
Michael Palmer <lawlux@gmail.com>
2017-10-11 19:57:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ get '/newteam' do
   erb :newteam
 end
 
+post '/team' do
+  @team_name = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg = params[:sg]
+  @pf = params[:pf]
+  @sf = params[:sf]
+  @c = params[:c]
+  erb :team
+end
 
 
 end
